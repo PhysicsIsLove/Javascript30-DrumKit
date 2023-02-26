b9452a379399641a3793a477cf1bc6e680bb5f74
PhysicsIsLove <aa.iitbbs@gmail.com>
2023-02-26 18:10:16 +0530

    after video lesson 1

--- 1_javascript_drum_kit/script.js
+++ 1_javascript_drum_kit/script.js
@@ -1,43 +1,38 @@
-console.log("Connected successsfully");
-
 document.addEventListener("keydown", onKeyDown);
-document.addEventListener("keyup", onKeyUp);
+// document.addEventListener("keyup", onKeyUp);
 
-function onKeyUp(event){
-    console.log("inside the key up function")
-    let keyPressed = event.code;
-    keyPressed = keyPressed.slice(keyPressed.length - 1, keyPressed.length);
-    updateTheStyle(keyPressed, false);
-    stopSound(keyPressed);
+const keys = document.querySelectorAll('.key');
+console.log("jkeys", keys);
+keys.forEach(key => {
+    console.log(key);
+    key.addEventListener("transitionend", onTransitionEnd)
+});
+
+function onTransitionEnd(event){
+    if(event.propertyName == "transform"){
+        event.target.classList.remove("playing");
+    } else{
+        return;
+    }
+    // console.log("transition end property event" , event);
 }
+
+// function onKeyUp(event){
+//     let keyPressed = event.code;
+//     keyPressed = keyPressed.slice(keyPressed.length - 1, keyPressed.length);
+//     document.getElementById("key" + keyPressed).classList.remove("playing");
+// }
 
 function onKeyDown(event){
-    console.log("On key rpess ", event);
     let keyPressed = event.code;
     keyPressed = keyPressed.slice(keyPressed.length - 1, keyPressed.length);
-    console.log("The final keypress is ", keyPressed);
-    // get the element whose content is that letter
-    updateTheStyle(keyPressed, true);
+    document.getElementById("key" + keyPressed).classList.add("playing");
     playSound(keyPressed);
-}
-
-function updateTheStyle(keyName, add){
-    const key = document.getElementById("key" + keyName);
-    if(add){        
-        key.classList.add("playing");
-    } else{
-        key.classList.remove("playing");
-    }
-    
 }
 
 function playSound(keyName){
     const audioELement = document.getElementById("sound" + keyName);
+    audioELement.currentTime = 0;
     audioELement.play();
 }
 
-function stopSound(keyName){
-    const audioELement = document.getElementById("sound" + keyName);
-    audioELement.pause();
-    audioELement.currentTime = 0;
-}
